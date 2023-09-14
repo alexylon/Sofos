06d1333b31cc267fed2760594dcc323a94836d5f
Alexander Alexandrov <contact@alexandrov.cc>
2023-09-15 00:18:13 +0300

    fix hiding send arrow

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -164,7 +164,7 @@ export default function Chat() {
                                         }
                                     },
                                 },
-                                endAdornment: isLoading && (
+                                endAdornment: !isLoading && (
                                     <InputAdornment position="end">
                                         <IconButton
                                             edge="end"
