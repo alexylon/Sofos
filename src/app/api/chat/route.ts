339d89dd7bd4540446781dfc4f01ff9df2bde8e1
Alexander Alexandrov <a.alexandrov@webpublication.fr>
2024-09-28 11:05:50 +0300

    Upgrade to 'ai' v3 and '@ai-sdk/openai'

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,29 +1,25 @@
-import { Configuration, OpenAIApi } from 'openai-edge'
-import { OpenAIStream, StreamingTextResponse } from 'ai'
+import { openai } from '@ai-sdk/openai';
+import { convertToCoreMessages, streamText } from 'ai';
 
-// Create an OpenAI API client (that's edge friendly!)
-const config = new Configuration({
-	apiKey: process.env.OPENAI_API_KEY
-})
-const openai = new OpenAIApi(config)
-
-// IMPORTANT! Set the runtime to edge
-export const runtime = 'edge'
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
 
 export async function POST(req: Request) {
 	// Extract the `messages` from the body of the request
-	const {messages} = await req.json()
+	const { messages } = await req.json();
 
-	// Ask OpenAI for a streaming chat completion given the prompt
-	const response = await openai.createChatCompletion({
-		model: 'gpt-4o-2024-08-06',
-		stream: true,
-		messages,
+	// Call the language model
+	const result = await streamText({
+		model: openai('gpt-4o'),
+		messages: convertToCoreMessages(messages),
 		temperature: 0.2,
-		top_p: 0.1,
-	})
-	// Convert the response into a friendly text-stream
-	const stream = OpenAIStream(response)
+		topP: 0.2,
+		async onFinish({ text, toolCalls, toolResults, usage, finishReason }) {
+			// implement your own logic here, e.g. for storing messages
+			// or recording token usage
+		},
+	});
+
 	// Respond with the stream
-	return new StreamingTextResponse(stream)
+	return result.toDataStreamResponse();
 }
