e0d8ada0ddfad2cccc7e9d24ca316d1a8728c33d
Alexander Alexandrov <self@alexan.dev>
2025-08-15 10:53:06 +0300

    Migrate 'react-markdown' v8 -> v10; add math rendering; refactor.

--- src/components/MarkdownText.tsx
+++ src/components/MarkdownText.tsx
@@ -1,86 +1,94 @@
-import React, { FC, memo } from "react";
-import ReactMarkdown, { Options } from "react-markdown";
+import React, { memo, useMemo, useCallback } from "react";
+import Markdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism";
 import { AppBar, Box, Toolbar } from "@mui/material";
 import { CopyToClipboardButton } from "@/components/CopyToClipboardButton";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
+import "katex/dist/katex.min.css"; // `rehype-katex` does not import the CSS for you
 
-const MemoizedReactMarkdown: FC<Options> = memo(
-	ReactMarkdown,
-	(prevProps, nextProps) =>
-		prevProps.children === nextProps.children &&
-		prevProps.className === nextProps.className
-)
+const MemoizedMarkdown = memo(Markdown);
 
-const MarkdownText = ({children}: any) => {
-	return (
-		<MemoizedReactMarkdown
-			components={{
-				code({node, inline, className, children, ...props}) {
-					const match = /language-(\w+)/.exec(className || '');
-					return !inline
-						? (
-							<Box>
-								<Box sx={{flexGrow: 1}}>
-									<AppBar
-										position="static"
-										color="primary"
-										sx={{
-											backgroundColor: '#777777',
-											borderRadius: '13px 13px 0 0',
-											zIndex: 'modal',
-											mb: '-10px'
-										}}
-										elevation={0}
-									>
-										<Toolbar variant="dense">
-											<Box sx={{display: 'flex'}}>
-												{(match && match[1]) || ''}
-											</Box>
-											<Box sx={{flexGrow: 10}} />
-											<Box sx={{display: 'flex', mr: -1}}>
-												<CopyToClipboardButton
-													value={String(children).replace(/\n$/, '')}
-													color="white"
-												/>
-											</Box>
-										</Toolbar>
-									</AppBar>
+const MarkdownText = ({ children }: any) => {
+	const remarkPlugins = useMemo(() => [remarkMath], []);
+	const rehypePlugins = useMemo(() => [rehypeKatex], []);
+
+	const CodeBlock = useCallback(
+		function CodeBlock({ node, className, children, ...rest }: any) {
+			const match = /language-(\w+)/.exec(className || "");
+			return match ? (
+				<Box>
+					<Box sx={{ flexGrow: 1 }}>
+						<AppBar
+							position="static"
+							color="primary"
+							sx={{
+								backgroundColor: "#777777",
+								borderRadius: "13px 13px 0 0",
+								zIndex: "modal",
+								mb: "-10px",
+							}}
+							elevation={0}
+						>
+							<Toolbar variant="dense">
+								<Box sx={{ display: "flex" }}>{(match && match[1]) || ""}</Box>
+								<Box sx={{ flexGrow: 10 }} />
+								<Box sx={{ display: "flex", mr: -1 }}>
+									<CopyToClipboardButton
+										value={String(children).replace(/\n$/, "")}
+										color="white"
+									/>
 								</Box>
-								<SyntaxHighlighter
-									{...props}
-									style={dracula}
-									customStyle={{borderRadius: '0 0 13px 13px'}}
-									language={(match && match[1]) || ''}
-									PreTag="div"
-									showLineNumbers={false}
-									codeTagProps={{
-										style: {
-											fontSize: '0.9rem',
-											fontFamily: 'var(--font-mono)'
-										}
-									}}
-								>
-									{String(children).replace(/\n$/, '')}
-								</SyntaxHighlighter>
-							</Box>
-						)
-						: (
-							<code
-								className={className}
-								{...props}
-								style={{
-									fontWeight: 'bold',
-								}}
-							>
-								{children}
-							</code>
-						);
-				},
-			}}
+							</Toolbar>
+						</AppBar>
+					</Box>
+					<SyntaxHighlighter
+						style={dracula}
+						customStyle={{ borderRadius: "0 0 13px 13px" }}
+						language={match[1]}
+						PreTag="div"
+						showLineNumbers={false}
+						codeTagProps={{
+							style: {
+								fontSize: "0.9rem",
+								fontFamily: "var(--font-mono)",
+							},
+						}}
+					>
+						{String(children).replace(/\n$/, "")}
+					</SyntaxHighlighter>
+				</Box>
+			) : (
+				<code
+					className={className}
+					{...rest}
+					style={{
+						fontWeight: "bold",
+					}}
+				>
+					{children}
+				</code>
+			);
+		},
+		[]
+	);
+
+	const components = useMemo(
+		() => ({
+			code: CodeBlock,
+		}),
+		[CodeBlock]
+	);
+
+	return (
+		<MemoizedMarkdown
+			remarkPlugins={remarkPlugins}
+			rehypePlugins={rehypePlugins}
+			components={components}
 		>
 			{children}
-		</MemoizedReactMarkdown>
+		</MemoizedMarkdown>
 	);
 };
 
